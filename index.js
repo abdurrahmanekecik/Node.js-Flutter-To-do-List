ae7a9891d0b65dc31561615d6165dad0b6ac7c48
Abdurrahman Ekecik <abdurrahmanekecik@gmail.com>
2023-01-07 15:36:33 +0300

    Mongodb Connect

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+console.log("Connected to db!");
+
+});
+
+app.listen(3000, () => {
+    console.log("App Started");
+});
 
 
-app.get('/',(req, res) => {
+
+
+app.get('/', (req, res) => {
     res.render('todo');
-    });
-    
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+});
 
 
 
-app.listen(3000, () => console.log("Server Up and running"));
+
