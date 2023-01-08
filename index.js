1e99d441abca057d225bc0ce91bf598169e55b8c
Abdurrahman Ekecik <abdurrahmanekecik@gmail.com>
2023-01-08 10:40:16 +0300

    Models Added

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
+const TodoTask = require("./models/TodoTask");
+
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv");
-dotenv.config();
-app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+const dotenv = require("dotenv");
+dotenv.config();
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
-console.log("Connected to db!");
-
+    console.log("Connected to db!");
 });
 
 app.listen(3000, () => {
     console.log("App Started");
 });
 
-
-
-
 app.get('/', (req, res) => {
     res.render('todo');
 });
 
-app.post('/', (req, res) => {
-    console.log(req.body);
+app.post('/', async (req, res) => {
+    const todoTask = TodoTask({
+        content: req.body.content
+    });
+    try {
+        await todoTask.save();
+        console.log("save.")
+        res.redirect("/");
+    }
+    catch (err) {
+        console.log(err);
+    }
+
 });
 
 
